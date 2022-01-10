662fe302571a18acb45d4efa5d427d2d47bbc86a
MrZulT <zulkiflee1250@gmail.com>
2022-01-10 21:54:11 +0700

    Add files via upload
    Add files via upload

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
     //toggle menu/navbar script
     $('.links-btn').click(function(){
         $('.navbar .links').toggleClass("active");
+        $('.links-btn i').toggleClass("active");
     });
   });
     
