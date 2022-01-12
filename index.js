046c3355107ca5935b27fc499151b34c48faf62b
MrZulT <zulkiflee1250@gmail.com>
2022-01-12 17:04:16 +0700

    Add files via upload
    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-      if(this.scrollY > 20){
-        $('.navbar').addClass("sticky");
-      }else{
-        $('.navbar').removeClass("sticky");
-      }
-        
-    });
-    
-    //toggle menu/navbar script
-    $('.links-btn').click(function(){
-        $('.navbar .links').toggleClass("active");
-        $('.links-btn i').toggleClass("active");
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $('.navbar').addClass("sticky");
+    } else {
+      $('.navbar').removeClass("sticky");
+    }
+
   });
-    
+
+  //toggle menu/navbar script
+  $('.links-btn').click(function () {
+    $('.navbar .links').toggleClass("active");
+    $('.links-btn i').toggleClass("active");
+  });
+
+  //owl-carousel script
+  $('.gallery').owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+      600: {
+        items: 2,
+        nav: false
+      },
+      1000: {
+        items: 3,
+        nav: false
+      }
+    }
+  });
+
+});
