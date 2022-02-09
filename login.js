8c3074bc364cfc421bf71c6062976d05da4463c2
MrZulT <zulkiflee1250@gmail.com>
2022-02-09 22:30:50 +0700

    Update

---
+++ login.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    //toggle menu/navbar script
+    $('.links-btn').click(function () {
+        $('.navbar .links').toggleClass("active");
+        $('.links-btn i').toggleClass("active");
+    });
+
+});
